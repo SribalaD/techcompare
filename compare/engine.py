ed55035394d84fb08b1b97d65774fc86e29bafa6
SribalaD <durvasulasribala@gmail.com>
2026-01-11 20:01:20 +0530

    Update engine.py

--- compare/engine.py
+++ compare/engine.py
@@ -1,1 +1,23 @@
+from compare.presets import PRESET_DATA
+from compare.textgen import generate_output
+
+def compare_options(option1, option2, criteria):
+    data1 = PRESET_DATA.get(option1.lower(), {})
+    data2 = PRESET_DATA.get(option2.lower(), {})
+
+    results = {"option1": option1, "option2": option2, "criteria": []}
+
+    for c in criteria:
+        score1 = data1.get(c, 2)
+        score2 = data2.get(c, 2)
+
+        results["criteria"].append({
+            "name": c,
+            "scores": {
+                option1: score1,
+                option2: score2,
+            }
+        })
+
+    return generate_output(results)
 
