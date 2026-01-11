1c214980f4a6690c201930f21141d069148037bd
SribalaD <durvasulasribala@gmail.com>
2026-01-11 20:02:13 +0530

    Create presets.py

---
+++ compare/presets.py
@@ -0,0 +1,14 @@
+PRESET_DATA = {
+    "firebase": {
+        "cost": 2,
+        "scalability": 3,
+        "auth": 3,
+        "ease": 3
+    },
+    "supabase": {
+        "cost": 3,
+        "scalability": 2,
+        "auth": 2,
+        "ease": 2
+    }
+}
